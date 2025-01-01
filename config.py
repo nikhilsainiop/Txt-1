66af7d81b75adc7cdc7d67bda3a3aa814e311ebf
nikhilsainiop <nikhilbhaiop02@gmail.com>
2025-01-01 13:12:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,13 @@
 import os
 
 # Fetch API ID and ensure it has a default value
-API_ID = int(os.environ.get("API_ID", "26513107"))  # Replace 12345 with your default
+API_ID = int(os.environ.get("API_ID", "28094744"))  # Replace 12345 with your default
 
 # Fetch API_HASH with a default empty string
-API_HASH = os.environ.get("API_HASH", "f14ce4b58dc8812cfc9665588472f2d4")
+API_HASH = os.environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
 
 # Fetch BOT_TOKEN with a default empty string
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "f14ce4b58dc8812cfc9665588472f2d4")
-
-# Fetch PASS_DB or use default value 721
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
-
-# Fetch OWNER or use 0 as a default
-OWNER = int(os.environ.get("OWNER", "1928404158"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7982396596:AAGZJZj1Gqc6XV46-9nXl-af1mhwAnLV6PU")
 
 # Define LOG variable properly
 LOG = True  # Set to False if logging is not required
