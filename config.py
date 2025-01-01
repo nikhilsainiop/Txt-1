84d983d2eeeec95ba6cb4aa45ecead885ec03699
nikhilsainiop <nikhilbhaiop02@gmail.com>
2025-01-01 13:48:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 # Fetch API ID and ensure it has a default value
-API_ID = int(os.environ.get("API_ID", "26513107"))  # Replace 12345 with your default
+API_ID = int(os.environ.get("API_ID", "28094744"))  # Replace 12345 with your default
 
 # Fetch API_HASH with a default empty string
-API_HASH = os.environ.get("API_HASH", "f14ce4b58dc8812cfc9665588472f2d4")
+API_HASH = os.environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
 
 # Fetch BOT_TOKEN with a default empty string
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "f14ce4b58dc8812cfc9665588472f2d4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7982396596:AAGZJZj1Gqc6XV46-9nXl-af1mhwAnLV6PU")
 
 # Fetch PASS_DB or use default value 721
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 # Fetch OWNER or use 0 as a default
-OWNER = int(os.environ.get("OWNER", "1928404158"))
+OWNER = int(os.environ.get("OWNER", "5680454765"))
 
 # Define LOG variable properly
 LOG = True  # Set to False if logging is not required
@@ -27,7 +27,7 @@ LOG = True  # Set to False if logging is not required
 # Admins list
 try:
     ADMINS = []
-    for x in (os.environ.get("ADMINS", "1928404158").split()):  # Default admin: 502980590
+    for x in (os.environ.get("ADMINS", "5680454765").split()):  # Default admin: 502980590
         ADMINS.append(int(x))
 except ValueError:
     raise Exception("Your Admins list does not contain valid integers.")
